06a7441354686d2e56c28758c335fd95b7d20de4
gary510 <46637741+gary510@users.noreply.github.com>
2019-05-26 16:00:48 -0700

    Add files via upload

---
+++ views/team_script.js
@@ -0,0 +1,94 @@
+/*
+ * Gary Luong
+ * Tristan Hilbert
+ *
+ * 5/19/2019
+ *
+ * AJAX script for Teams page of Sports Stats Plus
+ * 
+ */
+
+
+/* Function Definitions */
+
+
+function search(){
+    function appendResults(strHtml){
+        var container = document.getElementById("search-results");
+        container.innerHTML = strHtml;
+    }
+
+    var inputs = document.querySelectorAll("#team-search input")
+    var text = "";
+    for(var i = 0; i < inputs.length; i ++){
+        if(inputs[i].getAttribute("name") == "searchbar"){
+            text = inputs[i].value;
+            break;
+        }
+    }
+    if(text != "" && text != "Search Teams"){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = () => {
+            if(xhttp.readyState == 4 && xhttp.status == 200) {
+                appendResults(xhttp.responseText);
+            }
+        }
+        xhttp.open("GET", "/read/team-"+ text, true);
+        xhttp.send();
+    }else{
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = () => {
+            if(xhttp.readyState == 4 && xhttp.status == 200) {
+                appendResults(xhttp.responseText);
+            }
+        }
+        xhttp.open("GET", "/read/team", true);
+        xhttp.send();
+    }
+}
+
+
+function insertTeam(){
+
+    var inputs = document.querySelectorAll("#team-insert input")
+    var formInput = {};
+    for(var i = 0; i < inputs.length; i ++){
+        formInput[inputs[i].getAttribute("name")] = inputs[i].value;
+    }
+
+
+
+   inputObject = {};
+
+   inputObject.name = formInput["team-name"]; 
+   inputObject.location = formInput["team-loc"];       /* add required fields */
+   inputObject.color = formInput["team-color"];  /* add length requirement */
+   inputObject.coach = formInput["coach-name"];
+
+
+    
+    /* Send http request */
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = () => {
+        if(xhttp.readyState == 4 && xhttp.status == 200) {
+            window.location.replace("/teams.html");
+        }
+    }
+    xhttp.open("POST", "/create/team", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify(inputObject));
+}
+
+
+search();
+
+document.getElementById("team-search").addEventListener('submit', (e) => {
+    search();
+    e.preventDefault();
+}, false); 
+
+document.getElementById("team-insert").addEventListener('submit', (e) => {
+    insertTeam();
+    e.preventDefault();
+}, false); 
